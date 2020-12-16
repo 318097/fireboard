94683ef8217884aa3147a86aa2f7c078ee63799d
Mehul Lakhanpal <318097@gmail.com>
2020-12-16 17:46:59 +0530

    Add: Register

--- app/popup/src/components/Auth/Auth.js
+++ app/popup/src/components/Auth/Auth.js
@@ -7,50 +7,110 @@ import { setDataInStorage } from "../../utils";
 
 const Auth = ({ state, dispatch, setActivePage, setAppLoading }) => {
   const [data, setData] = useState({ username: "", password: "" });
+  const [authState, setAuthState] = useState("LOGIN");
 
   useEffect(() => {}, []);
 
   const setInputData = (update) => setData((prev) => ({ ...prev, ...update }));
 
-  const handleLogin = async () => {
+  const handleAuth = async () => {
     setAppLoading(true);
-    const { data: result } = await axios.post(`/auth/login`, data);
-    dispatch({
-      type: constants.SET_SESSION,
-      payload: { ...result, isLoggedIn: true },
-    });
-    setActivePage("DOT");
-    setAppLoading(false);
+    try {
+      if (authState === "LOGIN") {
+        const { data: result } = await axios.post(`/auth/login`, data);
+        dispatch({
+          type: constants.SET_SESSION,
+          payload: { ...result, isLoggedIn: true },
+        });
+        setActivePage("DOT");
+      } else {
+        const { data: result } = await axios.post(`/auth/register`, data);
+        console.log("Register::", result);
+        setActivePage("AUTH");
+        setAuthState("LOGIN");
+      }
+    } catch (err) {
+    } finally {
+      setAppLoading(false);
+    }
   };
 
-  return (
-    <section id="auth">
-      <div className="container">
-        <h3>Login</h3>
-        <Input
-          className="inp"
-          placeholder="Username"
-          name="username"
-          value={data.username}
-          onChange={(_, value) => setInputData(value)}
-        />
-        <Input
-          type="password"
-          className="inp"
-          placeholder="Password"
-          name="password"
-          value={data.password}
-          onChange={(_, value) => setInputData(value)}
-        />
-        <div className="button-wrapper">
-          <Button onClick={handleLogin} className="btn">
-            Login
-          </Button>
-          <div className="link">Register</div>
+  if (authState === "REGISTER")
+    return (
+      <section id="auth">
+        <div className="container">
+          <h3>Register</h3>
+          <Input
+            className="inp"
+            placeholder="Name"
+            name="name"
+            value={data.name}
+            onChange={(_, value) => setInputData(value)}
+          />
+          <Input
+            className="inp"
+            placeholder="Username"
+            name="username"
+            value={data.username}
+            onChange={(_, value) => setInputData(value)}
+          />
+          <Input
+            className="inp"
+            placeholder="Email"
+            name="email"
+            value={data.email}
+            onChange={(_, value) => setInputData(value)}
+          />
+          <Input
+            type="password"
+            className="inp"
+            placeholder="Password"
+            name="password"
+            value={data.password}
+            onChange={(_, value) => setInputData(value)}
+          />
+          <div className="button-wrapper">
+            <Button onClick={handleAuth} className="btn">
+              Register
+            </Button>
+            <div onClick={() => setAuthState("LOGIN")} className="link">
+              Login
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  else
+    return (
+      <section id="auth">
+        <div className="container">
+          <h3>Login</h3>
+          <Input
+            className="inp"
+            placeholder="Username"
+            name="username"
+            value={data.username}
+            onChange={(_, value) => setInputData(value)}
+          />
+          <Input
+            type="password"
+            className="inp"
+            placeholder="Password"
+            name="password"
+            value={data.password}
+            onChange={(_, value) => setInputData(value)}
+          />
+          <div className="button-wrapper">
+            <Button onClick={handleAuth} className="btn">
+              Login
+            </Button>
+            <div onClick={() => setAuthState("REGISTER")} className="link">
+              Register
+            </div>
+          </div>
+        </div>
+      </section>
+    );
 };
 
 export default Auth;
