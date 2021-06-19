e5e8e46d17ab58047110b1bda053085cd191b168
Mehul Lakhanpal <318097@gmail.com>
2021-06-19 14:29:01 +0530

    Refactor: getServerURL for config

--- app/popup/src/config.js
+++ app/popup/src/config.js
@@ -2,10 +2,20 @@ console.log("CONFIG:", __TYPE__, __ENV__);
 
 const isProd = __ENV__ === "PRODUCTION";
 
+const getServerURL = ({ isProd = false, serverType = "lambda" } = {}) => {
+  const connectToLambda = serverType === "lambda";
+  const LAMBDA_PROD =
+    "https://bubblegum-lambda.netlify.app/.netlify/functions/api";
+  const HEROKU_PROD = "https://bubblegum-server.herokuapp.com/api";
+  const LOCAL_SERVER = "http://localhost:7000/api";
+
+  if (isProd) return connectToLambda ? LAMBDA_PROD : HEROKU_PROD;
+
+  return LOCAL_SERVER;
+};
+
 const config = {
-  SERVER_URL: isProd
-    ? "https://bubblegum-server.herokuapp.com/api"
-    : "http://localhost:7000/api",
+  SERVER_URL: getServerURL({ isProd }),
   IS_LOCAL_STORAGE: __TYPE__ === "APP",
   DEFAULT_STATE: __TYPE__ === "APP",
   LOCAL_PROJECT_KEY: "dot-project-id",
