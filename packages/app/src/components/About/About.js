8dc3bcd7ad55695c6672c020dca098d3568cd65d
Mehul Lakhanpal <318097@gmail.com>
2021-09-18 21:40:47 +0530

    Fix: About page design

--- packages/app/src/components/About/About.js
+++ packages/app/src/components/About/About.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Anchor } from "@mantine/core";
 import { getProducts } from "@codedrops/lib/dist/downloads";
+import { copyToClipboard } from "@codedrops/lib";
+import notify from "../../lib/notify";
 import _ from "lodash";
 import "./About.scss";
 
@@ -26,6 +28,11 @@ const About = ({ appId }) => {
       });
   }, []);
 
+  const copy = () => {
+    copyToClipboard("mehullakhanpal@gmail.com");
+    notify("Copied!");
+  };
+
   // console.log("currentProduct::-", currentProduct, products);
 
   return (
@@ -35,18 +42,23 @@ const About = ({ appId }) => {
           <h3>Story</h3>
         </div>
         <div className="wrapper__fb">
-          The idea starts when I was working for a remote job and at one point
-          in time I was asked to log daily work. I used to write down from small
-          to big changes in code base, format it nicely and mailed it to my
-          manager. It became really frustating at a later point in time. Then I
-          got the idea why not make a simple application, in which developers
-          can write down simple tasks, track their daily work. I started work on
-          this application in 2019, but I was not sure on which platform to make
-          and should I even make it in the first case. As I got ideas and even
-          in my current company which does not ask me to log daily work, I just
-          felt the need to have a basic application which show me all the
-          pending tasks write from my project (No need for 3rd party tools like
-          Jira, Trello). And now we have Fireboard.
+          <p>
+            The idea kicked in when I was working for a remote job and at one
+            point in time I was asked to log daily work. I used to write down
+            from small to big changes in code base, format it nicely and mailed
+            it to my manager. It became really frustating at a later point in
+            time. Then I got the idea why not make a simple application, in
+            which developers can write down simple tasks, track their daily
+            work.
+          </p>
+          <p>
+            I started work on this application in 2019, but I was not sure on
+            which platform to make and should I even make it in the first case.
+            As I got ideas and even in my current company which does not ask me
+            to log daily work, I just felt the need to have a basic application
+            which show me all the pending tasks write from my project (No need
+            for 3rd party tools like Jira, Trello). And now we have Fireboard.
+          </p>
         </div>
       </div>
 
@@ -74,7 +86,11 @@ const About = ({ appId }) => {
           <h3>Contact us</h3>
         </div>
         <div className="wrapper__fb">
-          Reach out at mehullakhanpal@gmail.com for any queries
+          Reach out at{" "}
+          <span className="email__fb" onClick={copy}>
+            mehullakhanpal@gmail.com
+          </span>{" "}
+          for any queries
         </div>
       </div>
 
